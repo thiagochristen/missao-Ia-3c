52bf8a9086c199726d9dbfe503d77f913ade9b7f
thiagochristen <128715585+thiagochristen@users.noreply.github.com>
2024-09-30 09:30:38 -0300

    Create perguntas.js

---
+++ js/js/perguntas.js
@@ -0,0 +1,63 @@
+export const perguntas = [
+    {
+        enunciado: "Pergunta 1",
+        alternativas: [
+            {
+                texto: "Alternativa 1 da pergunta 1",
+                afirmacao: [
+                    "afirmacao 1",
+                    "afirmacao 2"
+                    ],
+                proxima: 1,
+            },
+            {
+                texto: "Alternativa 2 da pergunta 1",
+                afirmacao: [
+                    "afirmacao 1",
+                    "afirmacao 2"
+                    ],
+                proxima: 2,
+            }           
+            
+        ]
+    },
+    {
+        enunciado: "Pergunta 2",
+        alternativas: [
+            {
+                texto:"Alternativa 1 da pergunta 2",
+                afirmacao: [
+                    "afirmacao 1",
+                    "afirmacao 2"
+                    ]
+            },
+            {
+                texto: "Alternativa 2 da pergunta 2",
+                afirmacao: [
+                    "afirmacao 1",
+                    "afirmacao 2"
+                    ]
+            }
+        ]
+    },
+    {
+        enunciado: "Pergunta 3",
+        alternativas: [
+            {
+                texto:"Alternativa 1 da pergunta 3",
+                afirmacao: [
+                    "afirmacao 1",
+                    "afirmacao 2"
+                    ]
+            },
+            {
+                texto:"Alternativa 2 da pergunta 3",
+                afirmacao: [
+                    "afirmacao 1",
+                    "afirmacao 2"
+                    ]
+            }
+            
+        ]
+    },
+];

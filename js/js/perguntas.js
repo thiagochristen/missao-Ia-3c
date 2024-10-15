aee118f3bf6345f26e43f29957bec9f7a006e273
thiagochristen <128715585+thiagochristen@users.noreply.github.com>
2024-10-15 07:57:51 -0300

    Update perguntas.js

--- js/js/perguntas.js
+++ js/js/perguntas.js
@@ -1,63 +1,250 @@
 export const perguntas = [
     {
-        enunciado: "Pergunta 1",
+        enunciado: "Você Gosta de Cinema?",
         alternativas: [
             {
-                texto: "Alternativa 1 da pergunta 1",
+                texto: "Sim",
                 afirmacao: [
-                    "afirmacao 1",
-                    "afirmacao 2"
+                    "Muito bomm:)"
                     ],
-                proxima: 1,
+
+                    proxima: 1,
+            },
+
+            {
+                texto: "Não, eu prefiro Música",
+                afirmacao: [
+                    "Filmes as vezes podem parecer entendiantes, cada um tem seus gostos"
+                    ],
+                    proxima: 3,
+            }           
+            
+        ],
+
+    },
+    {
+        enunciado: "Você vê filmes com frequência?",
+        alternativas: [
+            {
+                texto:"Sim,Admiro filmes sou um verdadeiro fâ! :)",
+                afirmacao: 
+                    "Você possui muita cultura"
+                    ],
+
+                    proxima: 2,
+
             },
             {
-                texto: "Alternativa 2 da pergunta 1",
+                texto: "Não, prefiro ver outra coisa",
                 afirmacao: [
-                    "afirmacao 1",
-                    "afirmacao 2"
+                    "Tudo bem!",
+                    "Ta ok!"
                     ],
-                proxima: 2,
+                    proxima: 2,
+            }
+        ]
+    },
+    {
+        enunciado: "Você acha que as pessoas por trás dos filmes merecem mais atenção?",
+        alternativas: [
+            {
+                texto:"Sim",
+                afirmacao: [
+                    "Concordo totalmente com você!!",
+                    "Você possui uma visão muito boa"
+                    ],
+                    proxima: 4,
+            },
+            {
+                texto:"Não",
+                afirmacao: [
+                    "então blz brother",
+                    "Vai trabalhar e sai daqui então"
+                    ],
+            }
+            
+        ]
+    },
+    {
+        enunciado: "Você gosta de filmes de comédia",
+        alternativas: [
+            {
+                texto: "Sim",
+                afirmacao: [
+                    "Que legal eu também",
+                    "Te recomendo assistir o show de truman, é uma comédia com drama"
+                    ],
+                    proxima: 2,
+            },
+            {
+                texto: "Não",
+                afirmacao: [
+                    "Tudo bem",
+                    "Filha da mãe"
+                    ],
+                    proxima: 2,
             }           
             
         ]
     },
     {
-        enunciado: "Pergunta 2",
+        enunciado: "Você gosta do henry cavil ??",
         alternativas: [
             {
-                texto:"Alternativa 1 da pergunta 2",
+                texto: "Sim",
                 afirmacao: [
-                    "afirmacao 1",
-                    "afirmacao 2"
-                    ]
+                    "Bom gosto",
+                    "Eu também!!"
+                    ],
+                    proxima: 5,
             },
             {
-                texto: "Alternativa 2 da pergunta 2",
+                texto: "Não",
                 afirmacao: [
-                    "afirmacao 1",
-                    "afirmacao 2"
+                    "Mano por que ?",
+                    "Não venha mais aqui."
                     ]
-            }
-        ]
-    },
-    {
-        enunciado: "Pergunta 3",
-        alternativas: [
-            {
-                texto:"Alternativa 1 da pergunta 3",
-                afirmacao: [
-                    "afirmacao 1",
-                    "afirmacao 2"
-                    ]
-            },
-            {
-                texto:"Alternativa 2 da pergunta 3",
-                afirmacao: [
-                    "afirmacao 1",
-                    "afirmacao 2"
-                    ]
-            }
+            }           
             
         ]
     },
+    {
+        enunciado: "Gostaria de participar de um mini quiz sobre ele??",
+        alternativas: [
+            {
+                texto: "Sim, vamo lá vou",
+                afirmacao: [
+                    "Vamos ver se você sabe então",
+                    "VAMOSSSs"
+                    ],
+
+                    proxima: 6,
+            },
+            {
+                texto: "Não to afim",
+                afirmacao: [
+                    "tudo bem",
+                    "tudo bem então"
+                    ]
+            }           
+            
+        ]
+    },
+
+    {
+        enunciado: "Qual esporte ele praticava",
+        alternativas: [
+            {
+                texto: "rugby",
+                afirmacao: [
+                    "Boa!",
+                    "Acertou!"
+                    ],
+                
+                proxima: 7,
+            },
+            {
+                texto: "ping bong",
+                afirmacao: [
+                    "O cara não fuma não mano",
+                    "Você está errado, a proxima vai!"
+                    ],
+                proxima: 7,
+            }           
+            
+        ]
+    }, 
+    
+    {
+        enunciado: "Por que ele sofria bullingy na infancia?",
+        alternativas: [
+            {
+                texto: "Por ser muito inteligente",
+                afirmacao: [
+                    "errou feio errou rude",
+                    "ele era, mas não era isso"
+                    ],
+                
+                proxima: 8,
+            },
+            {
+                texto: "Por estar acima do peso",
+                afirmacao: [
+                    "ACERTOU, as pessoas não deveriam fazer isso",
+                    "Mandou bem!"
+                    ],
+                proxima: 8,
+            }           
+            
+        ]
+    },
+    
+
+{
+
+    enunciado: "Qual destes filmes ele não fez",
+        alternativas: [
+            {
+                texto: "Deadpool e Wolwerine",
+                afirmacao: [
+                    "Ele fez este, foi mal pelo spoiller",
+                    "Uma pequena aparação, perdão pelo spoiller",
+                    ],
+                proxima: 9,
+            },
+            {
+                texto: "Vovozona 4",
+                afirmacao: [
+                    "Nem tem vovozona 4.... eu saberia",
+                    "Seria legal mas não",
+                    ],
+                proxima: 9,
+            }          
+            
+        ]
+    },
+    {
+    enunciado: "Qual é o jogo favorito dele?",
+        alternativas: [
+            {
+                texto: "Warhammer 40000",
+                afirmacao: [
+                    "Acertou, essa era alto nivel",
+                    "VAAAPOOO acertou",
+                    ],
+                proxima: 10,
+            },
+            {
+                texto: "Street fighter",
+                afirmacao: [
+                    "Pior que não é",
+                    "errou feio errou rude",
+                    ],
+                proxima: 10,
+            }          
+            
+        ]
+    },
+
+    {
+        enunciado: "Gostou do quiz?",
+            alternativas: [
+                {
+                    texto: "Sim",
+                    afirmacao: [
+                        "VAPOOO ai sim meu fi!!!",
+                        "Obrigado pela participação!!!",
+                        ]
+                },
+                {
+                    texto: "Não",
+                    afirmacao: [
+                        "Se prepare para correr então",
+                        "Vou até a sua casa mudar essa opnião",
+                        ]
+                }          
+                
+            ]
+        },
+
 ];
